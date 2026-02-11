8c1bef465a97662b3317f5346718bee1393800cc
darya-lobova <dasha.lobova.it@gmail.com>
2026-02-11 17:10:58 +0300

    Add project files.

---
+++ FunctionRevisionProject/task01.cpp
@@ -0,0 +1,17 @@
+﻿// Задание 1: Добавьте прототип функции, которая принимает целочисленное 
+// и вещественное значения и возвращает их произведение.
+
+#include <iostream>
+using namespace std;
+
+float multiply(int a, float b);
+
+int main()
+{
+    cout << multiply(5, 3.5f);
+    return 0;
+}
+
+float multiply(int a, float b) {
+    return a * b;
+}
---
+++ FunctionRevisionProject/task02.cpp
@@ -0,0 +1,18 @@
+﻿// Задание 2: Объявите функцию `sum_large`, которая принимает небольшое 
+// и большое целые числа и возвращает их сумму.
+
+#include <iostream>
+using namespace std;
+
+long long sum_large(int a, long long b);
+
+int main()
+{
+    cout << sum_large(100, 10000000000LL);
+    return 0;
+}
+
+long long sum_large(int a, long long b) {
+
+    return a + b;
+}
